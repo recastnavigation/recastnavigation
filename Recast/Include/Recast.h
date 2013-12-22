64828488b19308016993795ade8cc97a46c1b7f0
Mikko Mononen <memononen@gmail.com>
2013-12-22 18:24:58 +0200

    Fixed docs of maxSimplificationError units.

--- Recast/Include/Recast.h
+++ Recast/Include/Recast.h
@@ -219,7 +219,7 @@ struct rcConfig
 	int maxEdgeLen;
 	
 	/// The maximum distance a simplfied contour's border edges should deviate 
-	/// the original raw contour. [Limit: >=0] [Units: wu]
+	/// the original raw contour. [Limit: >=0] [Units: vx]
 	float maxSimplificationError;
 	
 	/// The minimum number of cells allowed to form isolated island areas. [Limit: >=0] [Units: vx] 
