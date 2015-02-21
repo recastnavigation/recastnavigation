dd5b3fca83f89bb10c2930f87a4d9eebc7da38ca
Kromster80 <kromster80@gmail.com>
2015-02-21 15:19:14 +0300

    Remove vertex overrun fix

--- Recast/Source/RecastMesh.cpp
+++ Recast/Source/RecastMesh.cpp
@@ -747,7 +747,7 @@ static bool removeVertex(rcContext* ctx, rcPolyMesh& mesh, const unsigned short 
 	}
 	
 	// Remove vertex.
-	for (int i = (int)rem; i < mesh.nverts - 1; ++i)
+	for (int i = (int)rem; i < mesh.nverts - 2; ++i)
 	{
 		mesh.verts[i*3+0] = mesh.verts[(i+1)*3+0];
 		mesh.verts[i*3+1] = mesh.verts[(i+1)*3+1];
