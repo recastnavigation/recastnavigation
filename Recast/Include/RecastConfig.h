6de02f2492f82a6dac857a0f0b6a39e323995cff
Jakob Botsch Nielsen <Jakob.botsch.nielsen@gmail.com>
2017-06-27 12:07:10 +0200

    Move Recast configurations into RecastConfig.h

---
+++ Recast/Include/RecastConfig.h
@@ -0,0 +1,9 @@
+#ifndef RECASTCONFIG_H
+#define RECASTCONFIG_H
+
+/// Defines the number of bits allocated to rcSpan::smin and rcSpan::smax.
+/// If the mesh is appearing below the actual terrain, this parameter may
+/// need to be increased.
+static const int RC_SPAN_HEIGHT_BITS = 13;
+
+#endif // RECASTCONFIG_H
