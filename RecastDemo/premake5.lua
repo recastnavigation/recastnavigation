12e9ddb548840cec551894c1268e8ee723476853
Jarrett Chisholm <contact@jarrettchisholm.com>
2018-01-02 00:31:47 -0500

    fixed windows 32-bit and 64-bit build

--- RecastDemo/premake5.lua
+++ RecastDemo/premake5.lua
@@ -38,6 +38,12 @@ solution "recastnavigation"
 		platforms { "Win32", "Win64" }
 		defines { "WIN32", "_WINDOWS", "_CRT_SECURE_NO_WARNINGS", "_HAS_EXCEPTIONS=0" }
 
+	filter "platforms:Win32"
+		architecture "x32"
+
+	filter "platforms:Win64"
+		architecture "x64"
+
 	-- linux specific
 	configuration { "linux", "gmake" }
 		buildoptions {
