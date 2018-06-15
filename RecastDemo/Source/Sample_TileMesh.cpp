b9eff3379431e4882722927827a61a742869920b
Jakob Botsch Nielsen <Jakob.Botsch.Nielsen@gmail.com>
2018-06-16 01:33:36 +0200

    Remove unused code causing warning

--- RecastDemo/Source/Sample_TileMesh.cpp
+++ RecastDemo/Source/Sample_TileMesh.cpp
@@ -224,24 +224,6 @@ void Sample_TileMesh::cleanup()
 	m_dmesh = 0;
 }
 
-
-static const int NAVMESHSET_MAGIC = 'M'<<24 | 'S'<<16 | 'E'<<8 | 'T'; //'MSET';
-static const int NAVMESHSET_VERSION = 1;
-
-struct NavMeshSetHeader
-{
-	int magic;
-	int version;
-	int numTiles;
-	dtNavMeshParams params;
-};
-
-struct NavMeshTileHeader
-{
-	dtTileRef tileRef;
-	int dataSize;
-};
-
 void Sample_TileMesh::handleSettings()
 {
 	Sample::handleCommonSettings();
