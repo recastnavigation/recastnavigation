40cc13bba075604cf83f07d48348cb9f6cd84323
scripter <scripter.proger@gmail.com>
2018-10-11 19:37:36 +0300

    class NavmeshIslands

--- Detour/Include/DetourIslandSystem.h
+++ Detour/Include/DetourIslandSystem.h
@@ -24,10 +24,103 @@
 #ifndef DETOURISLANDSYSTEM_H
 #define DETOURISLANDSYSTEM_H
 
-#include <DetourNavMesh.h>
-#include <DetourNavMeshQuery.h>
+#include <cstring>
+#include "DetourAssert.h"
+#include "DetourNavMesh.h"
+#include "DetourNavMeshQuery.h"
 
 #ifdef NAVMESH_ISLAND_SYSTEM
+
+class NavmeshIslands
+{
+	struct TileIslands
+	{
+		inline void purge() { dtFree(islands); }
+		unsigned int* islands;
+		int nislands;
+		dtPolyRef base;
+	};
+
+	const dtNavMesh* m_nav;
+	TileIslands* m_tiles;
+	int m_ntiles;
+
+public:
+	NavmeshIslands() :
+		m_nav(0), m_tiles(0), m_ntiles(0)
+	{
+	}
+
+	~NavmeshIslands()
+	{
+		for (int i = 0; i < m_ntiles; ++i)
+			m_tiles[i].purge();
+		dtFree(m_tiles);
+	}
+
+	bool init(const dtNavMesh* nav)
+	{
+		m_nav = nav;
+
+		m_ntiles = nav->getMaxTiles();
+		if (!m_ntiles)
+			return false;
+		m_tiles = (TileIslands*)dtAlloc(sizeof(TileIslands)*m_ntiles, DT_ALLOC_TEMP);
+		if (!m_tiles)
+			return false;
+		memset(m_tiles, 0, sizeof(TileIslands)*m_ntiles);
+
+		// Alloc flags for each tile.
+		for (int i = 0; i < nav->getMaxTiles(); ++i)
+		{
+			const dtMeshTile* tile = nav->getTile(i);
+			if (!tile->header) continue;
+			TileIslands* ti = &m_tiles[i];
+			ti->nislands = tile->header->polyCount;
+			ti->base = nav->getPolyRefBase(tile);
+			if (ti->nislands)
+			{
+				ti->islands = (unsigned int*)dtAlloc(ti->nislands, DT_ALLOC_TEMP);
+				if (!ti->islands)
+					return false;
+				memset(ti->islands, 0, ti->nislands);
+			}
+		}
+
+		return true;
+	}
+
+	inline void clearAllIslands()
+	{
+		for (int i = 0; i < m_ntiles; ++i)
+		{
+			TileIslands* tf = &m_tiles[i];
+			if (tf->nislands)
+				memset(tf->islands, 0, tf->nislands);
+		}
+	}
+
+	inline unsigned int getIsland(dtPolyRef ref)
+	{
+		dtAssert(m_nav);
+		dtAssert(m_ntiles);
+		// Assume the ref is valid, no bounds checks.
+		unsigned int salt, it, ip;
+		m_nav->decodePolyId(ref, salt, it, ip);
+		return m_tiles[it].islands[ip];
+	}
+
+	inline void setFlags(dtPolyRef ref, unsigned int island)
+	{
+		dtAssert(m_nav);
+		dtAssert(m_ntiles);
+		// Assume the ref is valid, no bounds checks.
+		unsigned int salt, it, ip;
+		m_nav->decodePolyId(ref, salt, it, ip);
+		m_tiles[it].islands[ip] = island;
+	}
+
+};
 
 class dtIslandManager
 {
