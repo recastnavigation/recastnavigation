a8bdd07d4de4f204b4a3422ad862a394bc8bdea5
scripter <scripter.proger@gmail.com>
2018-10-11 22:32:58 +0300

    update NavmeshIslands/dtIslandManager

--- Detour/Include/DetourIslandSystem.h
+++ Detour/Include/DetourIslandSystem.h
@@ -29,10 +29,63 @@
 #include "DetourNavMesh.h"
 #include "DetourNavMeshQuery.h"
 
-#ifdef NAVMESH_ISLAND_SYSTEM
-
 class NavmeshIslands
 {
+public:
+	NavmeshIslands();
+	~NavmeshIslands();
+
+	bool init(const dtNavMesh* nav);
+	bool initTile(const int tileNum);
+	void resetTile(const int tileNum);
+
+	inline void clearAllIslands()
+	{
+		for (int i = 0; i < m_ntiles; ++i)
+		{
+			TileIslands* ti = &m_tiles[i];
+			if (ti->nislands)
+				memset(ti->islands, 0, ti->nislands);
+		}
+	}
+
+	inline unsigned int getIsland(const dtPolyRef ref) const
+	{
+		dtAssert(m_nav);
+		dtAssert(m_ntiles);
+		// Assume the ref is valid, no bounds checks.
+		unsigned int salt, it, ip;
+		m_nav->decodePolyId(ref, salt, it, ip);
+		return m_tiles[it].islands[ip];
+	}
+
+	inline unsigned int getIsland(const int tileNum, const int polyNum) const
+	{
+		dtAssert(m_nav);
+		dtAssert(m_ntiles);
+		return m_tiles[tileNum].islands[polyNum];
+	}
+
+	inline void setIsland(const dtPolyRef ref, const unsigned int island)
+	{
+		dtAssert(m_nav);
+		dtAssert(m_ntiles);
+		// Assume the ref is valid, no bounds checks.
+		unsigned int salt, it, ip;
+		m_nav->decodePolyId(ref, salt, it, ip);
+		m_tiles[it].islands[ip] = island;
+	}
+
+	inline void setIsland(const int tileNum, const int polyNum, const unsigned int island)
+	{
+		dtAssert(m_nav);
+		dtAssert(m_ntiles);
+		m_tiles[tileNum].islands[polyNum] = island;
+	}
+
+	void fillPolysWithIslandIdx(unsigned int fromIdx, unsigned int toIdx);
+
+private:
 	struct TileIslands
 	{
 		inline void purge() { dtFree(islands); }
@@ -44,104 +97,33 @@ class NavmeshIslands
 	const dtNavMesh* m_nav;
 	TileIslands* m_tiles;
 	int m_ntiles;
-
-public:
-	NavmeshIslands() :
-		m_nav(0), m_tiles(0), m_ntiles(0)
-	{
-	}
-
-	~NavmeshIslands()
-	{
-		for (int i = 0; i < m_ntiles; ++i)
-			m_tiles[i].purge();
-		dtFree(m_tiles);
-	}
-
-	bool init(const dtNavMesh* nav)
-	{
-		m_nav = nav;
-
-		m_ntiles = nav->getMaxTiles();
-		if (!m_ntiles)
-			return false;
-		m_tiles = (TileIslands*)dtAlloc(sizeof(TileIslands)*m_ntiles, DT_ALLOC_TEMP);
-		if (!m_tiles)
-			return false;
-		memset(m_tiles, 0, sizeof(TileIslands)*m_ntiles);
-
-		// Alloc flags for each tile.
-		for (int i = 0; i < nav->getMaxTiles(); ++i)
-		{
-			const dtMeshTile* tile = nav->getTile(i);
-			if (!tile->header) continue;
-			TileIslands* ti = &m_tiles[i];
-			ti->nislands = tile->header->polyCount;
-			ti->base = nav->getPolyRefBase(tile);
-			if (ti->nislands)
-			{
-				ti->islands = (unsigned int*)dtAlloc(ti->nislands, DT_ALLOC_TEMP);
-				if (!ti->islands)
-					return false;
-				memset(ti->islands, 0, ti->nislands);
-			}
-		}
-
-		return true;
-	}
-
-	inline void clearAllIslands()
-	{
-		for (int i = 0; i < m_ntiles; ++i)
-		{
-			TileIslands* tf = &m_tiles[i];
-			if (tf->nislands)
-				memset(tf->islands, 0, tf->nislands);
-		}
-	}
-
-	inline unsigned int getIsland(dtPolyRef ref)
-	{
-		dtAssert(m_nav);
-		dtAssert(m_ntiles);
-		// Assume the ref is valid, no bounds checks.
-		unsigned int salt, it, ip;
-		m_nav->decodePolyId(ref, salt, it, ip);
-		return m_tiles[it].islands[ip];
-	}
-
-	inline void setFlags(dtPolyRef ref, unsigned int island)
-	{
-		dtAssert(m_nav);
-		dtAssert(m_ntiles);
-		// Assume the ref is valid, no bounds checks.
-		unsigned int salt, it, ip;
-		m_nav->decodePolyId(ref, salt, it, ip);
-		m_tiles[it].islands[ip] = island;
-	}
-
 };
 
 class dtIslandManager
 {
 public:
-	dtIslandManager(dtNavMesh* navMesh);
+	dtIslandManager(dtNavMesh* navmesh);
+	~dtIslandManager();
 
 	void splitMeshToIslands(const dtQueryFilter* filter);
 	void splitTileToIslands(const int x, const int y, const dtQueryFilter* filter);
 
+	bool checkPathExists(const dtPolyRef startRef, const dtPolyRef endRef) const;
+
 private:
-	void fillPolysWithIslandIdx(unsigned int fromIdx, unsigned int toIdx, bool force);
 	void splitTileToIslands(dtNavMeshQuery* query, dtPolyRef* nearestPolys, int tileNum, const dtMeshTile* tile, const dtQueryFilter* filter);
 	void resetIndexes();
+
+	bool initNavmeshIslands();
+	void freeNavmeshIslands();
 
 	dtNavMeshQuery* getQuery() const;
 	void freeQuery(dtNavMeshQuery* query) const;
 
 private:
 	unsigned int m_freeIslandIdx;
-	const dtNavMesh* m_navMesh;
+	NavmeshIslands* m_navmeshIslands;
+	const dtNavMesh* m_navmesh;
 };
-#endif
 
 #endif // DETOURISLANDSYSTEM_H
