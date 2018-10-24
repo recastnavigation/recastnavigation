3926f2f9bc669c23dbf7d23ef9c91697f86ffb4d
Thomas Debesse <dev@illwieckz.net>
2018-10-25 01:20:03 +0200

    Revert "Make Detour and Recast use DAEMON_ASSERT"
    This reverts commit 6b68934d6d2715501e01b1e115413cefaa0aa7d3.

--- Recast/Include/RecastAssert.h
+++ Recast/Include/RecastAssert.h
@@ -22,7 +22,12 @@
 // Note: This header file's only purpose is to include define assert.
 // Feel free to change the file and include your own implementation instead.
 
-#include "common/Common.h"
-#define rcAssert DAEMON_ASSERT
+#ifdef NDEBUG
+// From http://cnicholson.net/2009/02/stupid-c-tricks-adventures-in-assert/
+#	define rcAssert(x) do { (void)sizeof(x); } while((void)(__LINE__==-1),false)  
+#else
+#	include <assert.h> 
+#	define rcAssert assert
+#endif
 
 #endif // RECASTASSERT_H
