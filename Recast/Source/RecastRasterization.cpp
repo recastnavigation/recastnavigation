ceebde75a74d5bf4dd64df1b39f46ac7676fe56b
Graham Pentheny <graham.pentheny@gmail.com>
2022-11-20 16:28:48 -0500

    Added docstring for rasterizeTri

--- Recast/Source/RecastRasterization.cpp
+++ Recast/Source/RecastRasterization.cpp
@@ -220,6 +220,23 @@ static void dividePoly(const float* in, int nin,
 	*nout2 = n;
 }
 
+///	Rasterize a single triangle to the heightfield.
+///
+///
+///	This code is extremely hot, so much care should be given to maintaining maximum perf here.
+/// 
+/// @param v0 Triangle vertex 0
+/// @param v1 Triangle vertex 1
+/// @param v2 Triangle vertex 2
+/// @param area The area ID to assign to the rasterized spans.
+/// @param hf Heightfield to rasterize into
+/// @param bmin The min extents of the heightfield bounding box.
+/// @param bmax The max extents of the heightfield bounding box.
+/// @param cs The x and z axis size of a voxel in the heightfield.
+/// @param ics 1 / cs
+/// @param ich 1 / ch
+/// @param flagMergeThr The threshold in which area flags will be merged. 
+/// @returns true if the operation completes successfully.  false if there was an error adding spans to the heightfield.
 static bool rasterizeTri(const float* v0, const float* v1, const float* v2,
 						 const unsigned char area, rcHeightfield& hf,
 						 const float* bmin, const float* bmax,
