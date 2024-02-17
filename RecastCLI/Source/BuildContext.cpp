214cdfa867f1bb810521a23a20c0f17c66a6fd83
Vandenbroucke Joran <joran.vandenbroucke@hotmail.com>
2024-02-17 10:55:33 +0100

    Fixed BuildContext looping forever Changed std::ranges::sort to std::sort in Filelist.cpp updated RecastCLI and Accuaracy.ps1 to run City, Military, and Zelda and compare it with a csv

--- RecastCLI/Source/BuildContext.cpp
+++ RecastCLI/Source/BuildContext.cpp
@@ -12,115 +12,104 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-BuildContext::BuildContext() :
-	m_messageCount(0),
-	m_textPoolSize(0)
-{
-	memset(m_messages, 0, sizeof(char*) * MAX_MESSAGES);
+BuildContext::BuildContext() : m_messageCount(0),
+                               m_textPoolSize(0) {
+    memset(m_messages, 0, sizeof(char *) * MAX_MESSAGES);
 
-	resetTimers();
+    resetTimers();
 }
 
 // Virtual functions for custom implementations.
-void BuildContext::doResetLog()
-{
-	m_messageCount = 0;
-	m_textPoolSize = 0;
+void BuildContext::doResetLog() {
+    m_messageCount = 0;
+    m_textPoolSize = 0;
 }
 
-void BuildContext::doLog(const rcLogCategory category, const char* msg, const int len)
-{
-	if (!len) return;
-	if (m_messageCount >= MAX_MESSAGES)
-		return;
-	char* dst = &m_textPool[m_textPoolSize];
-	const int n = TEXT_POOL_SIZE - m_textPoolSize;
-	if (n < 2)
-		return;
-	char* cat = dst;
-	char* text = dst+1;
-	const int maxtext = n-1;
-	// Store category
-	*cat = static_cast<char>(category);
-	// Store message
-	const int count = rcMin(len+1, maxtext);
-	memcpy(text, msg, count);
-	text[count-1] = '\0';
-	m_textPoolSize += 1 + count;
-	m_messages[m_messageCount++] = dst;
+void BuildContext::doLog(const rcLogCategory category, const char *msg, const int len) {
+    if (!len) return;
+    if (m_messageCount >= MAX_MESSAGES)
+        return;
+    char *dst = &m_textPool[m_textPoolSize];
+    const int n = TEXT_POOL_SIZE - m_textPoolSize;
+    if (n < 2)
+        return;
+    char *cat = dst;
+    char *text = dst + 1;
+    const int maxtext = n - 1;
+    // Store category
+    *cat = static_cast<char>(category);
+    // Store message
+    const int count = rcMin(len + 1, maxtext);
+    memcpy(text, msg, count);
+    text[count - 1] = '\0';
+    m_textPoolSize += 1 + count;
+    m_messages[m_messageCount++] = dst;
 }
 
-void BuildContext::doResetTimers()
-{
-	for (int i = 0; i < RC_MAX_TIMERS; ++i) {
-		m_accTime[i] = -1ll;
-	}
+void BuildContext::doResetTimers() {
+    for (int i = 0; i < RC_MAX_TIMERS; ++i) {
+        m_accTime[i] = -1ll;
+    }
 }
 
-void BuildContext::doStartTimer(const rcTimerLabel label)
-{
-	m_startTime[label] = getPerfTime();
+void BuildContext::doStartTimer(const rcTimerLabel label) {
+    m_startTime[label] = getPerfTime();
 }
 
-void BuildContext::doStopTimer(const rcTimerLabel label)
-{
-	const TimeVal endTime = getPerfTime();
-	const TimeVal deltaTime = endTime - m_startTime[label];
-	if (m_accTime[label] == -1)
-		m_accTime[label] = deltaTime;
-	else
-		m_accTime[label] += deltaTime;
+void BuildContext::doStopTimer(const rcTimerLabel label) {
+    const TimeVal endTime = getPerfTime();
+    const TimeVal deltaTime = endTime - m_startTime[label];
+    if (m_accTime[label] == -1)
+        m_accTime[label] = deltaTime;
+    else
+        m_accTime[label] += deltaTime;
 }
 
-int BuildContext::doGetAccumulatedTime(const rcTimerLabel label) const
-{
-	return getPerfTimeUsec(m_accTime[label]);
+int BuildContext::doGetAccumulatedTime(const rcTimerLabel label) const {
+    return getPerfTimeUsec(m_accTime[label]);
 }
 
-void BuildContext::dumpLog(const char* format, ...) const
-{
-	// Print header.
-	va_list ap;
-	va_start(ap, format);
-	vprintf(format, ap);
-	va_end(ap);
-	std::cout << std::endl;
+void BuildContext::dumpLog(const char *format, ...) const {
+    // Print header.
+    va_list ap;
+    va_start(ap, format);
+    vprintf(format, ap);
+    va_end(ap);
+    std::cout << std::endl;
 
-	// Print messages
-	constexpr int TAB_STOPS[4] = { 28, 36, 44, 52 };
-	for (int i = 0; i < MAX_MESSAGES; ++i) {
-		const char* message = m_messages[i];
-		int n = 0;
-		while(*message){
-			if (*message == '\t') {
-				int count = 1;
-				for (const int j : TAB_STOPS) {
-					if (n < j) {
-						count = j - n;
-						break;
-					}
-				}
-				while (--count) {
-					std::cout << ' ';
-					n++;
-				}
-			} else {
-				std::cout << *message;
-				n++;
-			}
-		}
-		std::cout << '\n';
-	}
-	std::cout << std::flush;
+    // Print messages
+    constexpr int TAB_STOPS[4] = {28, 36, 44, 52};
+    for (int i = 0; i < m_messageCount; ++i) {
+        const char *msg = m_messages[i] + 1;
+        int n = 0;
+        while (*msg) {
+            if (*msg == '\t') {
+                int count = 1;
+                for (const int j: TAB_STOPS) {
+                    if (n < j) {
+                        count = j - n;
+                        break;
+                    }
+                }
+                while (--count > 0) {
+                    std::cout << ' ';
+                    n++;
+                }
+            } else {
+                std::cout << *msg;
+                n++;
+            }
+            msg++;
+        }
+        std::cout << '\n';
+    }
+    std::cout << std::flush;
 }
 
-int BuildContext::getLogCount() const
-{
-	return m_messageCount;
+int BuildContext::getLogCount() const {
+    return m_messageCount;
 }
 
-const char* BuildContext::getLogText(const int i) const
-{
-	return m_messages[i]+1;
+const char *BuildContext::getLogText(const int i) const {
+    return m_messages[i] + 1;
 }
-
