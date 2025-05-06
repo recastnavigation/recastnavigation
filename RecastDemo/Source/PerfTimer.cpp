810e6f0b102b641fe40cf50db59f21bf351030ad
Graham Pentheny <graham.pentheny@gmail.com>
2025-05-06 10:39:25 -0400

    Clean up per-platform code in PerfTimer

--- RecastDemo/Source/PerfTimer.cpp
+++ RecastDemo/Source/PerfTimer.cpp
@@ -18,36 +18,35 @@
 
 #include "PerfTimer.h"
 
-#if defined(WIN32)
-
-// Win32
+#ifdef WIN32
 #	include <windows.h>
+#else
+#	include <sys/time.h>
+#endif
 
 TimeVal getPerfTime()
 {
+#ifdef WIN32
 	__int64 count;
 	QueryPerformanceCounter((LARGE_INTEGER*)&count);
 	return count;
+#else
+	timeval now;
+	gettimeofday(&now, 0);
+	return (TimeVal)now.tv_sec * 1000000L + (TimeVal)now.tv_usec;
+#endif
 }
 
 int getPerfTimeUsec(const TimeVal duration)
 {
+#ifdef WIN32
 	static __int64 freq = 0;
-	if (freq == 0) { QueryPerformanceFrequency((LARGE_INTEGER*)&freq); }
+	if (freq == 0)
+	{
+		QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+	}
 	return (int)(duration * 1000000 / freq);
-}
-
-#else  // Linux, BSD, OSX
-
-#	include <sys/time.h>
-
-TimeVal getPerfTime()
-{
-	timeval now;
-	gettimeofday(&now, 0);
-	return (TimeVal)now.tv_sec * 1000000L + (TimeVal)now.tv_usec;
-}
-
-int getPerfTimeUsec(const TimeVal duration) { return (int)duration; }
-
+#else
+	return (int)duration;
 #endif
+}
